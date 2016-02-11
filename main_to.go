b5d2aec7311c342b5929c5e110839e8cc33354fd
David Chen Billdal <David Chen Billdal>
2016-02-11 15:15:09 +0100

    Exercise 4

---
+++ main_to.go
@@ -0,0 +1,89 @@
+package main
+
+import(
+	"log"
+	"net"
+	"time"
+)
+
+func broadcastUdp(addr string){
+	udpAddr, err := net.ResolveUDPAddr("udp", addr)
+	if err != nil {log.Fatal(err)}
+
+	udpBroadcast, err := net.DialUDP("udp", nil, udpAddr)
+	if err != nil {log.Fatal(err)}
+
+	defer udpBroadcast.Close()
+
+	for{
+		udpBroadcast.Write([]byte("Not master"))
+		time.Sleep(100*time.Millisecond)
+	}
+}
+
+func listenUdp(port string, ipListChannel chan []string){
+	udpAddr, err := net.ResolveUDPAddr("udp", port)
+	if err != nil {log.Fatal(err)}
+
+	udpListen, err := net.ListenUDP("udp", udpAddr)
+	if err != nil {log.Fatal(err)}
+
+	defer udpListen.Close()
+
+	ipList := make([]string, 0)
+	var buffer[1024]byte
+
+	timer := make(chan bool, 1)
+	timeout := false
+
+	go timerout(timer)
+
+	for{
+		_, ipAddr, err := udpListen.ReadFromUDP(buffer[:])
+		if err != nil {log.Fatal(err)} 
+
+		if(!ipInList(ipAddr.String(), ipList)){
+			ipList = append(ipList, ipAddr.String())
+		}
+		log.Println(string(buffer[0:10]))
+
+		select{
+		case <-timer:
+			timeout = true;
+		default:
+			break;
+		}
+		if timeout{break;}
+		
+		time.Sleep(100*time.Millisecond)
+	}
+	ipListChannel <- ipList
+}
+
+func ipInList(ipAddr string, ipList []string) bool {
+    for _, b := range ipList {
+        if b == ipAddr {
+            return true
+        }
+    }
+    return false
+}
+
+func timerout(timer chan bool){
+	time.Sleep(2*time.Second)
+	timer <- true
+}
+
+func main(){
+	doneChannel := make(chan bool, 1)
+	ipListChannel := make(chan []string, 1)
+
+	port := ":20014"
+	broadcastAddr := "78.91.44.154:20014"
+
+	go broadcastUdp(broadcastAddr)
+	go listenUdp(port, ipListChannel)
+
+	log.Println(<-ipListChannel)
+	<-doneChannel
+}
